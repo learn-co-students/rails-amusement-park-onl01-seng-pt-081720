2d8c238a0d3bbc91cd38d7e81adb16dc1e736bd5
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-24 00:58:49 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+
+  #setting value only when the object is new and not persisted
+  #before_create will override existing value and we do not want to set default value if object already has values.
+  after_initialize :set_default, unless: :persisted?
+
+  def set_default
+    self.admin ||= false if self.admin.nil?
+  end
 end
--- db/migrate/20210124043325_add_admin_to_users.rb
+++ db/migrate/20210124043325_add_admin_to_users.rb
@@ -2,6 +2,7 @@ class AddAdminToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :admin, :boolean
     #Ex:- :default =>''
+    #Ex:- :default =>''
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
