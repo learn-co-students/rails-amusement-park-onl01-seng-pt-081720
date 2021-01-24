18774d632371e49e8e3dfbcef6b6dc86823b56c0
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-24 01:07:18 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ActiveRecord::Base
   #before_create will override existing value and we do not want to set default value if object already has values.
   after_initialize :set_default, unless: :persisted?
 
+  def mood
+    self.nausea > self.happiness ? mood = "sad" : mood = "happy"
+  end
+
+  private
+
   def set_default
     self.admin ||= false if self.admin.nil?
   end
